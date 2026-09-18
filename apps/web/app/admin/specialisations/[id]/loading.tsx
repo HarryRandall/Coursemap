e416bdcad5e30fdbf746b641a8260de7a682990c
Harry Randall <RandallHazza@gmail.com>
2026-09-18 14:30:56 +1000

    refactor: remove admin catalogue routes, import APIs and queue consumers

--- apps/web/app/admin/specialisations/[id]/loading.tsx
+++
@@ -1,4 +0,0 @@
-import { StructureReviewLoading } from "@/ui/admin/academic-structures/structure-review-loading";
-export default function Loading() {
-  return <StructureReviewLoading noun="specialisation" />;
-}
